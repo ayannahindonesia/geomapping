a60a250a4a98be832a42ae7cc2c0cf3ddb72dc1f
Rezi Apriliansyah <rapriliansyah@ayannah.com>
2019-09-02 17:00:09 +0700

    initial awal

--- migration/migration.go
+++ migration/migration.go
@@ -1,9 +1,9 @@
 package migration
 
 import (
+	"fmt"
 	"geomapping/asira"
 	"geomapping/models"
-	"fmt"
 	"strings"
 )
 
@@ -12,25 +12,94 @@ func Seed() {
 	defer seeder.Commit()
 
 	if asira.App.ENV == "development" {
-		// seed bank types
-		bankTypes := []models.BankType{
-			models.BankType{
-				Name:        "BPD",
-				Description: "Layanan BPD",
+		// seed province
+		province := []models.Province{
+			models.Province{
+				Name: "DKI Jakarta",
 			},
-			models.BankType{
-				Name:        "BPR",
-				Description: "Layanan BPR",
+			models.Province{
+				Name: "Jawa Barat",
 			},
-			models.BankType{
-				Name:        "Koperasi",
-				Description: "Layanan Koperasi",
+			models.Province{
+				Name: "Sumatera Selatan",
 			},
 		}
-		for _, bankType := range bankTypes {
-			bankType.Create()
+		for _, prov := range province {
+			prov.Create()
 		}
 
+		// seed province
+		city := []models.City{
+			models.City{
+				Name:       "Jakarta Selatan",
+				ProvinceID: 1,
+			},
+			models.City{
+				Name:       "Jakarta Barat",
+				ProvinceID: 1,
+			},
+			models.City{
+				Name:       "Jakarta Pusat",
+				ProvinceID: 1,
+			},
+			models.City{
+				Name:       "Jakarta Utara",
+				ProvinceID: 1,
+			},
+			models.City{
+				Name:       "Bandung",
+				ProvinceID: 2,
+			},
+			models.City{
+				Name:       "Palembang",
+				ProvinceID: 3,
+			},
+		}
+		for _, k := range city {
+			k.Create()
+		}
+
+		district := []models.District{
+			models.District{
+				Name:   "Mampang Prapatan",
+				CityID: 1,
+			},
+			models.District{
+				Name:   "Setiabudi",
+				CityID: 1,
+			},
+			models.District{
+				Name:   "Sekip",
+				CityID: 6,
+			},
+		}
+		for _, kec := range district {
+			kec.Create()
+		}
+
+		village := []models.Village{
+			models.Village{
+				Name:       "Mampang Prapatan",
+				DistrictID: 1,
+				ZipCode:    12790,
+				AreaCode:   "31.74.03.1001",
+			},
+			models.Village{
+				Name:       "Kuningan Barat",
+				DistrictID: 1,
+				ZipCode:    12710,
+				AreaCode:   "31.74.03.1005",
+			},
+			models.Village{
+				Name:       "Sekip Jaya",
+				DistrictID: 3,
+				ZipCode:    30128,
+				AreaCode:   "16.71.09.1003",
+			},
+		}
+		for _, kel := range village {
+			kel.Create()
+		}
 	}
 }
 
@@ -39,10 +108,10 @@ func Truncate(tableList []string) (err error) {
 	if len(tableList) > 0 {
 		if tableList[0] == "all" {
 			tableList = []string{
-				"kelurahan",
-				"kecamatan",
-				"kota",
-				"provinsi",
+				"villages",
+				"districts",
+				"cities",
+				"provinces",
 			}
 		}
 
---
+++ models/village.go
@@ -0,0 +1,45 @@
+package models
+
+type (
+	Village struct {
+		BaseModel
+		Name       string `json:"name" gorm:"column:name;type:varchar(255)"`
+		DistrictID int    `json:"district_id" gorm:"column:district_id"`
+		ZipCode    int    `json:"zip_code" gorm:"column:zip_code"`
+		AreaCode   string `json:"area_code" gorm:"column:area_code;type:varchar(255)"`
+	}
+)
+
+func (p *Village) Create() (*Village, error) {
+	err := Create(&p)
+	return p, err
+}
+
+func (p *Village) Save() (*Village, error) {
+	err := Save(&p)
+	return p, err
+}
+
+func (p *Village) Delete() (*Village, error) {
+	err := Delete(&p)
+	return p, err
+}
+
+func (p *Village) FindbyID(id int) (*Village, error) {
+	err := FindbyID(&p, id)
+	return p, err
+}
+
+func (p *Village) PagedFilterSearch(page int, rows int, orderby string, sort string, filter interface{}) (result PagedSearchResult, err error) {
+	kelurahan := []Village{}
+	result, err = PagedFilterSearch(&kelurahan, page, rows, orderby, sort, filter)
+
+	return result, err
+}
+
+func (p *Village) GetAll(filter interface{}) (result interface{}, err error) {
+	vill := []Village{}
+	result, err = GetAll(&vill, filter)
+
+	return result, err
+}
